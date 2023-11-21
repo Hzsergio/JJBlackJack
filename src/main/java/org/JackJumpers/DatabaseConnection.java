bb4893ad85a166a90f28959d968b9d5d61b69338
RamosER77 <146763918+RamosER77@users.noreply.github.com>
2023-11-21 08:13:04 -0800

    erubiel

--- src/main/java/org/JackJumpers/DatabaseConnection.java
+++ src/main/java/org/JackJumpers/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/blackjackdb";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "hernandez";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
